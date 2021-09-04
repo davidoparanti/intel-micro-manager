d9ccd90ed8ccfa10acf651bdea3a931e5a39d089
David Oparanti <david.oparanti@gmail.com>
2021-09-04 13:40:11 +0100

    Add SecurityConstant

---
+++ src/main/java/com/example/intelmicromanager/constant/SecurityConstant.java
@@ -0,0 +1,15 @@
+package com.example.intelmicromanager.constant;
+
+public class SecurityConstant {
+    public static final long EXPIRATION_TIME = 5 * 60 * 60 * 1000; //5 days
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String JWT_TOKEN_HEADER = "Jwt_Token";
+    public static final String TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified";
+    public static final String INTEL_MANAGER = "Intel Manager Plc";
+    public static final String INTEL_MANAGER_ADMINISTRATION = "User management portal";
+    public static final String AUTHORITIES = "authorities";
+    public static final String FORBIDDEN_MESSAGE = "You need to login to access this page";
+    public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to access this page";
+    public static final String OPTION_HTTP_METHOD = "Options";
+    public static final String[] PUBLIC_URL = {"/use/login", "user/register", "user/resetpassword/**", "/user/image/**"};
+}
