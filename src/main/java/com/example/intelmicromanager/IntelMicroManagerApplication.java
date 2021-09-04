b5fafe2896ee582f72b6b62aa685a6c49311d60d
David Oparanti <david.oparanti@gmail.com>
2021-09-04 20:43:04 +0100

    Implement SecurityConfiguration

--- src/main/java/com/example/intelmicromanager/IntelMicroManagerApplication.java
+++ src/main/java/com/example/intelmicromanager/IntelMicroManagerApplication.java
@@ -2,12 +2,20 @@ package com.example.intelmicromanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class IntelMicroManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(IntelMicroManagerApplication.class, args);
+    }
+
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
