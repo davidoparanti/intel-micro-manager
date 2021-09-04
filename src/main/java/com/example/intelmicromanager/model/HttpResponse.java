7a352742e7c34a23be6fa1b5ca55ebf26999bc34
David Oparanti <david.oparanti@gmail.com>
2021-09-04 12:08:34 +0100

    Add HttpResponse domain

---
+++ src/main/java/com/example/intelmicromanager/model/HttpResponse.java
@@ -0,0 +1,19 @@
+package com.example.intelmicromanager.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class HttpResponse {
+    private int httpStatusCode;
+    private HttpStatus httpStatus;
+    private String reason;
+    private String message;
+
+}
